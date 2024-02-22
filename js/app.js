9bfd61dda5ff94609440d01e87e6fa666a268b5d
Abdelouahab Djoudi <info@djoudi.net>
2024-02-22 15:21:05 +0100

    update

---
+++ js/app.js
@@ -0,0 +1,119 @@
+// var  const operation comparition condition boucle function
+
+var num1 = 24 //number
+var num2 = 36  // numbrt
+
+var num1 =24,num2 = 36
+var num3
+num3 = 78
+
+var num4 
+num4  = 29.4  // number   // float double 
+
+var str  = "Ecoin" // String
+var ch = 'A'   // String  Char
+var isAdmin = true  // Boolean
+var x 
+//undefined
+//console.log(typeof num4)  // api 
+
+//document.write(str)
+//alert(str)
+
+var x = 28
+x = 16
+
+const PI = 3.14
+
+//console.log(PI);
+
+var x=2 , 
+    y=14
+    ,z
+   // console.log(x);
+  x =  x + y   // x+=y
+  x =  x - y   // x-=y
+  x =  x * y   // x*=y
+  x =  x / y   // x/=y
+
+  var i  = 9
+
+  i = i + 1  // i++
+  i = i - 1  // i--
+
+   // console.log(x);
+
+
+    x = '14'
+    y = 14
+    isAdmin = false
+   // < <= > >= == ===
+   // console.log(!isAdmin);
+
+   // console.log(i**2);
+  //var c = '25' + '36'
+//console.log(c);
+    // var c = prompt('please enter your num C')
+    // c = parseInt(c)
+    // console.log(typeof c);
+
+
+// var level = 5
+// // var base = 10
+// level = level+ 
+
+
+
+// nextLevel = level-- * base 
+//console.log(level);
+
+//condition type relation comp
+// AND OR 
+// x = 30
+// if(x > 13)
+//     console.log("Welcom > 13");
+//fdsfdfds
+//
+// else if(x < 13)
+//     console.log("Welcom < 13");
+//
+// else{
+//     console.log("Not Work");
+// }
+
+
+// array object
+
+
+// ((x>13) && (x > 16) && (x >= 25))
+
+// true && false ===> false 
+// false && true ===> false 
+// false && false ===> false 
+// true && true ===> true 
+
+// true || false ===> true 
+// false || true ===> true 
+// false || false ===> false 
+// true || true ===> true 
+
+
+ //num 1 num2 op + - */
+
+//  var num1 = parseInt(prompt("please enter Num1"))
+//  var num2 = parseInt(prompt("please enter Num2"))
+
+//  var op = prompt("Your op")
+// if(typeof num1 == "Number" && typeof num2 == "Number" ){
+//     if(op=="+") console.log(num1 + num2);
+//     else if(op == "-") console.log(num1 - num2);
+//     else if(op == "*") console.log(num1 * num2);
+//     else if(op == "/"){
+//         if(num2 == 0) console.log("Div By Zero");
+//         else console.log(num1 / num2);
+//     }
+//     else console.log('No operation');
+
+// }else{
+//     console.log("please enter Number")
+// }
